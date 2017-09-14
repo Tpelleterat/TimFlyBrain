8e2f5cda8877114678c052f0b65c7217065e8773
tpelleterat <tpelleterat@NS-BDX-DEV31>
2017-09-14 14:14:53 +0200

    Status managment

--- TimFlyBrain/Managers/GlobalManager.cs
+++ TimFlyBrain/Managers/GlobalManager.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimFlyBrain.Models;
 
 namespace TimFlyBrain.Managers
 {
     public class GlobalManager
     {
+        BrainStatusEnum status = BrainStatusEnum.ControllerNotConnected;
         ArduinoManager _arduinoManager;
         ClientManager _clientManager;
         bool _isInitialized;
@@ -42,6 +44,8 @@ namespace TimFlyBrain.Managers
                 return;
 
             _arduinoManager = new ArduinoManager();
+            _arduinoManager.OnArduinoConnected += OnArduinoConnected;
+            _arduinoManager.OnArduinoDisconnected += OnArduinoDisconnected;
             _arduinoManager.StartConnectionLoop();
 
             _clientManager = new ClientManager();
@@ -49,10 +53,41 @@ namespace TimFlyBrain.Managers
             _clientManager.OnRollChange += _clientManager_OnRollChange;
             _clientManager.OnPitchChange += _clientManager_OnPitchChange;
             _clientManager.OnInitialization += _clientManager_OnInitialization;
+            _clientManager.OnAskStatus += _clientManager_OnAskStatus;
 
             await _clientManager.Init();
 
             _isInitialized = true;
+        }
+
+        private async void _clientManager_OnAskStatus(object sender, EventArgs e)
+        {
+            await SendStatus();
+        }
+
+        private void OnArduinoDisconnected(object sender, EventArgs e)
+        {
+            status = BrainStatusEnum.ControllerNotConnected;
+
+        }
+
+        private void OnArduinoConnected(object sender, EventArgs e)
+        {
+            status = BrainStatusEnum.Initialisation;
+        }
+
+        private async void ChangeStatus(BrainStatusEnum newStatus)
+        {
+            status = newStatus;
+            await SendStatus();
+        }
+
+        private async Task SendStatus()
+        {
+            if (_clientManager.IsConnected)
+            {
+                await _clientManager.SendStatus(status.ToString());
+            }
         }
 
         private void _clientManager_OnElevationChange(object sender, string e)
